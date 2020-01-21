cc3dce584fc86037c1e680967e0f634c70eab357
Samuel Förström <samppa.f@gmail.com>
2020-01-21 16:04:21 +0200

    BaseForGraphEditor

---
+++ Scripts/DataStructures/Graph/DirectionalGraph/DirectionalGraph.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// (Cyclic) Directed Graph. Useful for creating predefined path or networks
+/// </summary>
+public class DirectionalGraph : MonoBehaviour {
+
+  public class Node {
+    Vector3 position;
+    Node[] branches;
+  }
+}
