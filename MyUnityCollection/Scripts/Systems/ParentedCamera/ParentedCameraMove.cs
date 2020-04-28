8d72b8942f241328a0814d1815f2200ac708fe08
Samuel <32210043+Satsaa@users.noreply.github.com>
2020-04-28 13:37:32 +0300

    Add parented camera

---
+++ MyUnityCollection/Scripts/Systems/ParentedCamera/ParentedCameraMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParentedCameraMove : MonoBehaviour {
+  public float speed = 0.0025f;
+  public bool multiplyByZoom = true;
+  public KeyCode key = KeyCode.Mouse2;
+
+  ParentedCamera pc;
+  float prevX = 0;
+  float prevY = 0;
+  bool isDragging = false;
+  void Start() {
+    pc = gameObject.GetComponent<ParentedCamera>();
+  }
+
+  // Update is called once per frame
+  void Update() {
+    // Drag rotate
+    if (Input.GetKeyDown(key)) {
+      isDragging = true;
+      prevX = Input.mousePosition.x;
+      prevY = Input.mousePosition.y;
+    } else if (Input.GetKeyUp(key)) {
+      isDragging = false;
+    }
+    if (isDragging) {
+
+      pc.displacement += pc.transform.right * (prevX - Input.mousePosition.x) * speed * (multiplyByZoom ? pc.distance : 1);
+      pc.displacement += pc.transform.up * (prevY - Input.mousePosition.y) * speed * (multiplyByZoom ? pc.distance : 1);
+
+      prevX = Input.mousePosition.x;
+      prevY = Input.mousePosition.y;
+    }
+  }
+}
---
+++ MyUnityCollection/Scripts/Systems/ParentedCamera/ParentedCameraRotate.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParentedCameraRotate : MonoBehaviour {
+  public float rotSpeed = 1f;
+  public KeyCode key = KeyCode.Mouse0;
+  public bool useHotkey = false;
+  public bool limitVertRot = true;
+  public float rotationLimit = 89.9f;
+
+  ParentedCamera pc;
+  float prevX = 0;
+  float prevY = 0;
+  bool isDragging = false;
+  void Start() {
+    pc = gameObject.GetComponent<ParentedCamera>();
+  }
+
+  // Update is called once per frame
+  void Update() {
+    // Drag rotate
+    if (useHotkey) {
+      if (Input.GetKeyDown(key)) {
+        isDragging = true;
+        prevX = Input.mousePosition.x;
+        prevY = Input.mousePosition.y;
+      } else if (Input.GetKeyUp(key)) {
+        isDragging = false;
+      }
+    } else {
+      isDragging = true;
+    }
+
+    if (isDragging) {
+      pc.horRot -= (prevX - Input.mousePosition.x) * rotSpeed;
+      pc.verRot += (prevY - Input.mousePosition.y) * rotSpeed;
+      if (limitVertRot) {
+        pc.verRot = Mathf.Clamp(pc.verRot, -rotationLimit, rotationLimit);
+      }
+      prevX = Input.mousePosition.x;
+      prevY = Input.mousePosition.y;
+    }
+  }
+}
