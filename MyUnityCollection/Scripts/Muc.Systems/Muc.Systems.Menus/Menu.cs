976239f156b52bde191be8fc60c2e652c805267f
Satsaa <32210043+Satsaa@users.noreply.github.com>
2023-04-13 12:55:47 +0300

    Add Menus system from KalsiumHeroes

---
+++ MyUnityCollection/Scripts/Muc.Systems/Muc.Systems.Menus/Menu.cs
@@ -0,0 +1,118 @@
+
+namespace Muc.Systems.Interaction {
+
+	using System;
+	using System.Linq;
+	using System.Collections.Generic;
+	using UnityEngine;
+	using Object = UnityEngine.Object;
+
+	public class Menu : MonoBehaviour {
+
+		[field: SerializeField, Tooltip("Used to compare Menus. Many settings define their behaviour by comparing types of Menus. Some menus can extends the comparison beyond just this.")]
+		public string type { get; private set; }
+
+		[field: SerializeField, Tooltip("Will this Menu close the previous chain of Menus that are replaceable?")]
+		public bool replace { get; private set; }
+
+		[field: SerializeField, Tooltip("Will this Menu be closed when a Replacing Menu is added after this?")]
+		public bool replaceable { get; private set; }
+
+		[field: SerializeField, Tooltip("Disallow multiple consecutive Menus of this type? The newest one is used.")]
+		public bool collapse { get; private set; }
+
+		[field: SerializeField, Tooltip("Reuse this Menu for upcoming Menus of the same type? No per-menu data is stored.")]
+		public bool reuse { get; private set; }
+
+		[field: SerializeField, Tooltip("Don't destroy the last instance of a Menu of this type, complementing the reuse toggle.")]
+		public bool persist { get; private set; }
+
+		[field: SerializeField, Tooltip("Don't hide the Menu when it is not at the top?")]
+		public bool alwaysVisible { get; private set; }
+
+		[field: SerializeField, Tooltip("Does the close key defined by Menus close this menu?")]
+		public bool allowCloseKey { get; private set; } = true;
+
+		/// <summary> True if the Menu should be destroyed after the hide animation. </summary>
+		[field: SerializeField, HideInInspector]
+		public bool destroy { get; set; }
+
+		[field: SerializeField, HideInInspector]
+		public bool visible { get; protected set; }
+
+
+		public static bool CompareType(Menu a, Menu b) {
+			return a.CompareType(b) && b.CompareType(a);
+		}
+
+		protected virtual bool CompareType(Menu other) {
+			return this.type == other.type;
+		}
+
+		public virtual void OnHide() {
+			if (visible) {
+				visible = false;
+				if (TryGetComponent<Animator>(out var animator)) {
+					animator.SetTrigger("Hide");
+				}
+			}
+		}
+
+		public virtual void OnShow() {
+			if (!visible) {
+				visible = true;
+				gameObject.SetActive(true);
+				if (TryGetComponent<Animator>(out var animator)) {
+					animator.SetTrigger("Show");
+				}
+			}
+		}
+
+		public void FinalizeHide() {
+			if (destroy) Destroy(gameObject);
+			else if (!visible) gameObject.SetActive(false);
+		}
+
+	}
+
+	#if UNITY_EDITOR
+	namespace Editors {
+
+		using System;
+		using System.Linq;
+		using System.Collections.Generic;
+		using UnityEngine;
+		using UnityEditor;
+		using Object = UnityEngine.Object;
+		using Menu = Muc.Systems.Interaction.Menu;
+		using static Muc.Editor.PropertyUtil;
+		using static Muc.Editor.EditorUtil;
+
+		[CanEditMultipleObjects]
+		[CustomEditor(typeof(Menu), true)]
+		public class MenuEditor : Editor {
+
+			Menu t => (Menu)target;
+
+			SerializedProperty reuse;
+
+			void OnEnable() {
+				reuse = serializedObject.FindProperty(GetBackingFieldName(nameof(Menu.reuse)));
+			}
+
+			public override void OnInspectorGUI() {
+				serializedObject.Update();
+
+				if (reuse.boolValue) {
+					DrawPropertiesExcluding(serializedObject, "m_Script");
+				} else {
+					DrawPropertiesExcluding(serializedObject, "m_Script", GetBackingFieldName(nameof(Menu.persist)));
+				}
+
+				serializedObject.ApplyModifiedProperties();
+			}
+		}
+	}
+	#endif
+
+}
---
+++ MyUnityCollection/Scripts/Muc.Systems/Muc.Systems.Menus/Menus.cs
@@ -0,0 +1,193 @@
+
+namespace Muc.Systems.Interaction {
+
+	using System;
+	using System.Linq;
+	using System.Collections.Generic;
+	using UnityEngine;
+	using Muc.Components.Extended;
+	using Object = UnityEngine.Object;
+
+	public class Menus : Singleton<Menus> {
+
+		new public static Transform transform => instance.gameObject.transform;
+
+		public List<Menu> menus;
+		public List<Menu> cached;
+
+
+		/// <summary>
+		/// Removes the top-most instance of target menu.
+		/// Due to reusage of Menus this may result in unexpected behaviour.
+		/// Use the Hide(index) for removal of a specific instance of a Menu.
+		/// </summary>
+		public static bool Hide(Menu target) => instance._Hide(target);
+
+		/// <summary> Removes the Menu at index. </summary>
+		public static void Hide(int index) => instance._Hide(index);
+
+		/// <summary> Removes the last/newest Menu. </summary>
+		public static void Pop() => instance._Hide(instance.menus.Count - 1);
+
+		/// <summary> Shows a menu representing the source Menu and optionally runs an initializer on it before it is activated. </summary>
+		public static Menu Show(Menu source, Action<Menu> initializer = null) => instance._Show(source, initializer);
+
+
+		public void TryClose() {
+			if (menus.Count > 0 && menus.Last().allowCloseKey) {
+				Pop();
+			}
+		}
+
+		private bool _Hide(Menu target) {
+			var mi = menus.FindLastIndex(v => v == target);
+			if (mi != -1) {
+				Hide(mi);
+				return true;
+			}
+			return false;
+		}
+
+		private void _Hide(int index, bool collapse = true) {
+			var menu = menus[index];
+			var destroy = true;
+
+			// Persist for reuse
+			if (destroy && menu.reuse && menu.persist) {
+				for (int i = 0; i < menus.Count; i++) {
+					var other = menus[i];
+					if (i != index && Menu.CompareType(menu, other)) {
+						goto skipPersist;
+					}
+				}
+				destroy = false;
+				if (!cached.Contains(menu)) cached.Add(menu);
+			}
+		skipPersist:
+
+			// Check if reused
+			if (destroy && menu.reuse) {
+				for (int i = 0; i < menus.Count; i++) {
+					var other = menus[i];
+					if (i != index && other.reuse && Menu.CompareType(menu, other)) {
+						destroy = false;
+						goto skipReuse;
+					}
+				}
+			}
+		skipReuse:
+
+			menu.destroy = destroy;
+			menus.RemoveAt(index);
+			menu.OnHide();
+
+			if (collapse) {
+				var offset = menus.Count - index;
+				while (TryCollapse(menus.Count - offset)) ;
+			}
+
+			var last = menus.LastOrDefault();
+			if (last != null && !last.alwaysVisible) {
+				last.OnShow();
+			}
+		}
+
+		private Menu _Show(Menu source, Action<Menu> initializer = null) {
+			Menu instance = null;
+
+			// reuse
+			if (source.reuse) {
+				var ci = cached.FindIndex(v => Menu.CompareType(source, v));
+				if (ci != -1) {
+					instance = cached[ci];
+					cached.RemoveAt(ci);
+					initializer?.Invoke(instance);
+				} else {
+					var mi = menus.FindIndex(v => v.reuse && Menu.CompareType(source, v));
+					if (mi != -1) {
+						instance = menus[mi];
+						initializer?.Invoke(instance);
+					}
+				}
+			}
+
+			// default instance
+			if (instance == null) {
+				instance = Instantiate(source, transform);
+				initializer?.Invoke(instance);
+			}
+
+			menus.Add(instance);
+			instance.OnShow();
+
+			while (TryCollapse(menus.Count - 1) || TryReplace(menus.Count - 1)) { }
+
+			if (menus.Count >= 2) {
+				var before = menus[^2];
+				if (!before.alwaysVisible) {
+					before.OnHide(); // No destroy...
+				}
+			}
+
+			return instance;
+		}
+
+		private bool TryCollapse(int index) {
+			if (menus.Count > index && index > 0) {
+				var after = menus[index];
+				var before = menus[index - 1];
+				if (Menu.CompareType(after, before) && after.collapse && before.collapse) {
+					_Hide(index - 1, false);
+					return true;
+				}
+			}
+			return false;
+		}
+
+		private bool TryReplace(int index) {
+			if (menus.Count > index && index > 0) {
+				var menu = menus[index];
+				if (menu.replaceable) {
+					_Hide(index, false);
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+
+
+	#if UNITY_EDITOR
+	namespace Editors {
+
+		using System;
+		using System.Linq;
+		using System.Collections.Generic;
+		using UnityEngine;
+		using UnityEditor;
+		using Object = Object;
+		using static Muc.Editor.PropertyUtil;
+		using static Muc.Editor.EditorUtil;
+
+		[CanEditMultipleObjects]
+		[CustomEditor(typeof(Menus), true)]
+		public class MenusEditor : Editor {
+
+			Menus t => (Menus)target;
+
+			public override void OnInspectorGUI() {
+				serializedObject.Update();
+
+				DrawDefaultInspector();
+
+				if (GUILayout.Button("Pop")) {
+					Menus.Pop();
+				}
+
+				serializedObject.ApplyModifiedProperties();
+			}
+		}
+	}
+	#endif
+
+}
